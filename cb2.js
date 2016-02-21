dd4f529df4c2dfbf4bd7104823add290c9de9f08
David Wadden <dave.wadden@gmail.com>
2016-02-21 11:10:55 -0500

    Use declarations rather than vars for functions
    Apparently Crockford is against this. I think it makes the code a bit
    more readable.

--- cb2.js
+++ cb2.js
@@ -4,130 +4,104 @@
 // $(document).ready(function() { driverLoop(); });
 
 // The driver loop
-var driverLoop = function() {
-    setupSlider();
+function driverLoop() {
     var buffer = makeBuffer();
     var grid = makeGrid(buffer);
     registerKeyPress(grid);
     grid.scan();
-};
+}
 
 // when a key is pressed, dispatch it to the grid
-var registerKeyPress = function(grid) {
+function registerKeyPress(grid) {
     $(document)[0].onkeypress = function(event) { grid.action(event); };
-};
-
-var setupSlider = function() {
-    var s = $("#slider");
-    var stop = function(event) {
-        $("#slider-value")[0].innerText = $(this).slider("value");
-    };
-    s.slider({ min: 0,
-               max: 50,
-               value: 20,
-               stop: stop});
-};
+}
 
 // grid constructor
-var makeGrid = function(inputBuffer) {
+// TODO: Accept an initializer object instead of an argument.
+function makeGrid(inputBuffer) {
     var gridElem = $("#grid");
     var rowElems = gridElem.find(".rowOff");
     var buffer = inputBuffer;             // a passed-in buffer object
-    var initRows = function(rowElems) {
+    function initRows(rowElems) {
         var curried = curry(makeRow, inputBuffer);
         return rowElems.toArray().map(curried); // jquery's map is wrong; us the ES5 version
-    };
+    }
     var rows = initRows(rowElems);
     var rowIx = 0;         // index of current highlighted row
     var timeout = null;
-    var nRows = function() {
-        return rows.length;
-    };
-    var getRows = function() {
-        return rows;
-    };
-    var nextRow = function() {
-        rowIx = (rowIx + 1) % nRows();
-    };
-    var resetRow = function() {
-        rowIx = 0;
-    };
-    var resetTimeout = function() {
+    function nRows() { return rows.length; }
+    function getRows() { return rows; }
+    function nextRow() { rowIx = (rowIx + 1) % nRows(); }
+    function resetRow() { rowIx = 0; }
+    function resetTimeout() {
         clearTimeout(timeout);
         timeout = null;
-    };
-    var scan = function() {     // scan the rows
+    }
+    function scan() {     // scan the rows
         var currentRow = rows[rowIx];
         currentRow.toggle();
         var currentButton = currentRow.getIdButton();
         currentButton.toggle();
         currentButton.say();
-        var callback = function() {
+        function callback() {
             currentRow.toggle();
             currentButton.toggle();
             nextRow();
             scan();
-        };
+        }
         timeout = setTimeout(callback, 1000);
-    };
-    var action = function(event) {
+    }
+    function action(event) {
         resetTimeout();
         var currentRow = rows[rowIx];
-        var onReturnNoSelection = function() {
+        function onReturnNoSelection() {
             currentRow.toggle();
             nextRow();
+            // TODO: write a function for this.
             $(document)[0].onkeypress = function(event) { action(event); };
             scan();
-        };
-        var onReturnSelection = function() {
+        }
+        function onReturnSelection() {
             currentRow.toggle();
             resetRow();
             $(document)[0].onkeypress = function(event) { action(event); };
             scan();
-        };
-        var callback1 = function() {
+        }
+        function callback1() {
             var idButton = currentRow.getIdButton();
             idButton.say();
-            var callback2 = function() {
+            function callback2() {
                 idButton.toggle();
                 $(document)[0].onkeypress = function(event) { currentRow.action(event, onReturnSelection); };
                 currentRow.scan(onReturnNoSelection);
-            };
+            }
             setTimeout(callback2, 1000);
-        };
+        }
         setTimeout(callback1, 250);
-    };
+    }
     return Object.freeze({nRows, getRows, nextRow, scan, action});
-};
+}
 
 // row constructor
-var makeRow = function(inputBuffer, rowInput) {
+// TODO: Accept an initializer object the place of inputBuffer
+function makeRow(inputBuffer, rowInput) {
     var rowElem = rowInput;
     var buttonElems = $(rowElem).find(".btnOff");
     var buffer = inputBuffer;
-    var makeButtons = function(buttonElems) {
+    // TODO: abstract this pattern?
+    function makeButtons(buttonElems) {
         var curried = curry(makeCommButton, buffer);
         return buttonElems.toArray().map(curried); // as above, jquery map() is wrong
-    };
+    }
     var buttons = makeButtons(buttonElems);
     var buttonIx = 0;      // index of current highlighted button
     var timeout = null;
-    var getIdButton = function() {
-        return buttons[0];
-    };
-    var getCommandButtons = function() {
-        return buttons.slice(1);
-    };
-    var nCommandButtons = function() {
-        return getCommandButtons().length;
-    };
-    var nextButton = function() {
-        buttonIx = (buttonIx + 1) % nCommandButtons();
-    };
-    var resetButton = function() {
-        buttonIx = 0;
-    };
-    var toggle = function() {
+    function getIdButton() { return buttons[0]; }
+    function getCommandButtons() { return buttons.slice(1); }
+    function nCommandButtons() { return getCommandButtons().length; }
+    function nextButton() { buttonIx = (buttonIx + 1) % nCommandButtons(); }
+    function resetButton() { buttonIx = 0; }
+    function toggle() {
         var toAdd, toRemove;
         var rowq = $(rowElem);
         if (rowq.attr("class") === "rowOff") {
@@ -139,55 +113,56 @@ var makeRow = function(inputBuffer, rowInput) {
         }
         rowq.addClass(toAdd);
         rowq.removeClass(toRemove);
-    };
-    var scan = function(fromCaller) {
+    }
+    function scan(fromCaller) {
         var currentButton = getCommandButtons()[buttonIx];
         currentButton.toggle();
         currentButton.say();
-        var callback;
-        var tailCall = ((buttonIx < nCommandButtons() - 1) ?
-                        function () { scan(fromCaller); } :
-                        fromCaller);
-        callback = function() {
+        function callback() {
+            var tailCall = ((buttonIx < nCommandButtons() - 1) ?
+                            function () { scan(fromCaller); } :
+                            fromCaller);
             currentButton.toggle();
             nextButton();
             tailCall();
-        };
+        }
         timeout = setTimeout(callback, 1000); // Advance to next button
-    };
-    var resetTimeout = function() {
+    }
+    function resetTimeout() {
         clearTimeout(timeout);
         timeout = null;
-    };
-    var action = function(event, fromCaller) {
+    }
+    function action(event, fromCaller) {
         resetTimeout();
         var currentButton = getCommandButtons()[buttonIx];
-        var callback1 = function() {
+        function callback1() {
             currentButton.action();
-            var callback2 = function() {
+            function callback2() {
                 currentButton.toggle();
                 resetButton();
                 fromCaller();
-            };
+            }
             setTimeout(callback2, 1000);
-        };
+        }
         setTimeout(callback1, 250);
-    };
+    }
     return Object.freeze({getIdButton, getCommandButtons, nextButton,
                           toggle, scan, action});
-};
+}
 
 // console button constructor
-var makeCommButton = function(inputBuffer, buttonInput) {
+// TODO: Accept an initializer object in the place of the input buffer
+function makeCommButton(inputBuffer, buttonInput) {
     var buttonElem = buttonInput;
     var buttonText = buttonElem.value;
     var buffer = inputBuffer;
-    var say = function() {
+    function say() {
         var utterance = new window.SpeechSynthesisUtterance(buttonText);
         utterance.lang = "en-US";
         window.speechSynthesis.speak(utterance);
-    };
-    var toggle = function() {
+    }
+    // TODO: refactor this. it's redundant
+    function toggle() {
         var toAdd, toRemove;
         var buttonq = $(buttonElem);
         if (buttonq.attr("class") === "btnOff") {
@@ -199,95 +174,61 @@ var makeCommButton = function(inputBuffer, buttonInput) {
         }
         buttonq.addClass(toAdd);
         buttonq.removeClass(toRemove);
-    };
-    var getText = function() {
-        return buttonText;
-    };
-    var action = function() {
+    }
+    function getText() { return buttonText; }
+    function action() {
         say();
         buffer.push(buttonText);
-    };
+    }
     return Object.freeze({say, toggle, getText, action});
-};
+}
 
 // menu constructor
-var makeMenu = function() {
+function makeMenu() {
     ;
-};
+}
+
+function makeMenuButton() {
+    ;
+}
+
+function makeSlider() {
+    var sliderElem = $("#slider");
+    var sliderValue = $("#slider-value");
+    function stop(event) {
+        $("#slider-value")[0].innerText = $(this).slider("value");
+    }
+    s.slider({ min: 0,
+               max: 50,
+               value: 20,
+               stop: stop});
+}
 
 // buffer constructor
-var makeBuffer = function() {
+function makeBuffer() {
     var bufferElem = $("#textBuffer");
     var textElem = bufferElem.find("p");
     var bufferText = "";
-    var getContents = function() {
-        return bufferText;
-    };
-    var update = function() {
-        textElem.text(bufferText);
-    };
-    var push = function(character) {
+    function getContents() { return bufferText; }
+    function update() { textElem.text(bufferText); }
+    function push(character) {
         bufferText += character;
         update();
-    };
-    var backspace = function() {
+    }
+    function pop() {
         bufferText = bufferText.slice(0, -1);
         update();
-    };
-    var clear = function() {
+    }
+    function clear() {
         bufferText = "";
         update();
-    };
-    return Object.freeze({getContents, push, backspace, clear});
-};
-
-var setupGrid = function() {
-    var grid = {};
-    grid.buttons = $("input");           // get the buttons
-    grid.nButtons = grid.buttons.length; // number of buttons in grid
-    grid.ix = 0;
-    grid.increment = function() {
-        this.ix = (this.ix + 1) % this.nButtons;
-    };
-    grid.reset = function() {
-        this.ix = 0;
-    };
-    grid.scan = function() {
-        var that = this;         // a hack since "this" is dumb
-        var button = this.buttons[this.ix];
-        button.className = "btnOn";
-        readOut(button.value);
-        var callback = function() {
-            button.className = "btnOff";
-            that.increment();
-            that.scan();
-        };
-        this.timeout = setTimeout(callback, 1000);
-    };
-    grid.delegate = function(event) {
-        clearTimeout(this.timeout);
-        var that = this;
-        var button = this.buttons[this.ix];
-        readOut(button.value);
-        var callback = function() {
-            button.className = "btnOff";
-            that.reset();
-            that.scan();
-        };
-        this.timeout = setTimeout(callback, 1000);
-    };
-    var pause = function() {
-        console.log("paused");
-    };
-    grid.actions = {112: pause};
-    // Register handler to clear grid timeout
-    $(document)[0].onkeypress = function(event) { grid.delegate(event); };
-    return grid;
-};
+    }
+    return Object.freeze({getContents, push, pop, clear});
+}
 
 // Helper functions
 function curry(func, ...first) {
     return function(...second) {
         return func(...first, ...second);
     };
-};
+}
