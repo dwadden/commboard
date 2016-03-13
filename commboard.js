8ec6feb27c4bc5f040726b8fc2c1e1e8e0977f41
David Wadden <dave.wadden@gmail.com>
2016-03-12 21:37:52 -0500

    Clean up helper functions

--- commboard.js
+++ commboard.js
@@ -871,34 +871,31 @@ function makeSlider() {
     return that;
 }
 
+// *****************************************************************************
 
+// Helper procedures
 
-// Procedure to speak text out loud
+// Speak text out loud
 function speak(text) {
     let utterance = new window.SpeechSynthesisUtterance(text);
     utterance.lang = LANG;
     window.speechSynthesis.speak(utterance);
     return utterance;
 }
-
 function notImplemented () {
     throw new Error("Not implemented.");
 }
-
-// Helper functions
 function curry(func, ...first) {
     return function(...second) {
         return func(...first, ...second);
     };
 }
-
 // Return an array consisting of element x repeated n times.
 function repeat(x, n) {
     let xs = new Array(n);
     xs.fill(x);
     return xs;
 }
-
 // Pad (on right) array xs with value fill, such that the returned array has
 // length "len"
 function pad(xs, fillVal, len) {
@@ -909,12 +906,10 @@ function pad(xs, fillVal, len) {
     let padding = repeat(fillVal, nMissing);
     return xs.concat(padding);
 }
-
 // Convert first letter of word to uppercase
 function capitalize(text) {
     return text[0].toUpperCase() + text.slice(1);
 }
-
 // Zip two arrays. If unequal lengths, truncate the longer.
 function zip(xs, ys) {
     if (xs.length === 0 || ys.length === 0) {
