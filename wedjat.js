6135f4ffa5e290b9107c155b1db5fd371f81b885
David Wadden <dave.wadden@gmail.com>
2016-03-20 06:02:28 -0400

    Store user name along with email and password

--- wedjat.js
+++ wedjat.js
@@ -894,23 +894,29 @@ function registerEmailConfigButton() {
     function clicked() {
         let alert = `WARNING: This is an experimental feature. The email
 password will not be stored securely, and it is possible a malicious person
-could retrieve them. Only enter a password for an account created expressely for
+could retrieve it. Only enter a password for an account created expressely for
 use with this program, which will NEVER be used to exchange sensitive
 information (bank / credit card statements, travel documents, etc).`;
-        bootbox.alert(alert, getUserName);
+        bootbox.alert(alert, getName);
     }
-    function getUserName() {
+    function getName() {
+        bootbox.prompt("Please enter your name.",
+                       function(name) { getEmailAddress(name); });
+
+    }
+    function getEmailAddress(name) {
         bootbox.prompt("Please enter your email address.",
-                       function(username) { getPassword(username); });
+                       function(address) { getPassword(name, address); });
     }
-    function getPassword(username) {
+    function getPassword(name, address) {
         bootbox.prompt("Please enter your password.",
                        function(password) {
-                           storeEmailConfig(username, password);
+                           storeEmailConfig(name, address, password);
                        });
     }
-    function storeEmailConfig(username, password) {
-        window.sessionStorage.setItem("username", username);
+    function storeEmailConfig(name, address, password) {
+        window.sessionStorage.setItem("name", name);
+        window.sessionStorage.setItem("address", address);
         window.sessionStorage.setItem("password", password);
     }
 }
