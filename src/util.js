ecc59e43d843e105642155d55bab2fb5c991e637
David Wadden <dave.wadden@gmail.com>
2016-07-14 23:03:27 -0400

    Get rid of unused functions in util.

--- src/util.js
+++ src/util.js
@@ -3,25 +3,10 @@
 // Helper procedures
 
 // Exports
-module.exports = { notImplemented,
-                   curry,
-                   repeat,
+module.exports = { repeat,
                    pad,
                    capitalize,
-                   pass,
                    renameKeys };
-
-function notImplemented () {
-    // Throw not implemented error.
-    throw new Error("Not implemented.");
-}
-
-function curry(func, ...first) {
-    // Curry a function.
-    return function(...second) {
-        return func(...first, ...second);
-    };
-}
 
 function repeat(x, n) {
     // Return an array consisting of element x repeated n times.
@@ -44,10 +29,6 @@ function pad(xs, fillVal, len) {
 function capitalize(text) {
     // Convert first letter of word to uppercase
     return text[0].toUpperCase() + text.slice(1);
-}
-
-function pass() {
-    // Do nothing.
 }
 
 function renameKeys(obj, keys) {
